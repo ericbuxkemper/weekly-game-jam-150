e0570aa0112658c76272d2701aaa139ab507a108
Eric <ericbuxkemper@gmail.com>
2020-05-22 21:44:08 -0500

    added scenes for each level

--- Assets/Scripts/RonaPawn.cs
+++ Assets/Scripts/RonaPawn.cs
@@ -1,18 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class RonaPawn : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class RonaPawn : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/SceneLoaderUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderUI : MonoBehaviour
+{
+    private Coroutine _loadSceneRoutine = null;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        if (_loadSceneRoutine == null)
+            _loadSceneRoutine = StartCoroutine(WaitForSceneToLoad(sceneName));
+    }
+
+    private IEnumerator WaitForSceneToLoad(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (true)
+        {
+            if (asyncLoad.isDone)
+            {
+                // start loading screen animation
+                Debug.Log("Loading Scene" + sceneName);
+                _loadSceneRoutine = null;
+                break;
+            }
+
+            yield return null;
+        }
+    }
+}
